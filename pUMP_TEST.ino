e339c816da0903eed2bab9d01d9dd62d339605f9
cjowilson <47095769+cjowilson@users.noreply.github.com>
2019-02-22 13:57:46 -0600

    Add files via upload

---
+++ pUMP_TEST.ino
@@ -0,0 +1,24 @@
+
+const int motorA=5;
+const int motorArev=6;
+ 
+   void setup() {
+  // put your setup code here, to run once:
+   //Sets up motor inputs
+    Serial.begin(9600);
+    pinMode(5,OUTPUT );
+    pinMode(6,OUTPUT);
+  
+}
+
+void loop() {
+//Turn motor clockwise for 2 seconds
+digitalWrite(motorA,HIGH);
+digitalWrite(motorArev,LOW);
+delay(5000);
+//Turn motor counter-clockwise for 2 seconds
+digitalWrite(motorA,LOW);
+digitalWrite(motorArev,HIGH);
+delay(5000);
+  }
+
