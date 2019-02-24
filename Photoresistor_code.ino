17a3e02a97fda625308f24704097b9880964fcba
cjowilson <47095769+cjowilson@users.noreply.github.com>
2019-02-24 11:52:38 -0600

    Add files via upload

---
+++ Photoresistor_code.ino
@@ -0,0 +1,15 @@
+int lightPin = A0;  //define a pin for Photo resistor
+int ledPin=11;     //define a pin for LED
+
+void setup()
+{
+    Serial.begin(9600);  //Begin serial communcation
+    pinMode( ledPin, OUTPUT );
+}
+
+void loop()
+{
+    Serial.println(analogRead(lightPin)); //Write the value of the photoresistor to the serial monitor.
+    analogWrite(ledPin, analogRead(lightPin));
+    delay(10);
+}
