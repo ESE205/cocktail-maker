e1e5de9100ff2aa89cbc8007f3138c739d08f7e2
cjowilson <47095769+cjowilson@users.noreply.github.com>
2019-03-03 12:44:53 -0600

    Add files via upload

---
+++ Sensor_testor.ino
@@ -0,0 +1,78 @@
+/*
+SparkFun Inventor’s Kit
+Circuit 3B-Distance Sensor
+Control the color of an RGB LED using an ultrasonic distance sensor.
+This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+This code is completely free for any use.
+View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v40
+Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
+*/
+
+const int trigPin = 11;           //connects to the trigger pin on the distance sensor       
+const int echoPin = 12;           //connects to the echo pin on the distance sensor     
+
+const int redPin = 3;             //pin to control the red LED inside the RGB LED
+const int greenPin = 5;           //pin to control the green LED inside the RGB LED
+const int bluePin = 6;            //pin to control the blue LED inside the RGB LED
+
+float distance = 0;               //stores the distance measured by the distance sensor
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calculatedDistance;         //variable to store the distance calculated from the echo time
+  
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calculatedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+  
+  return calculatedDistance;              //send back the distance that was calculated
+}
+
+void setup()
+{
+  Serial.begin (9600);        //set up a serial connection with the computer
+
+  pinMode(trigPin, OUTPUT);   //the trigger pin will output pulses of electricity 
+  pinMode(echoPin, INPUT);    //the echo pin will measure the duration of pulses coming back from the distance sensor
+
+  //set the RGB LED pins to output
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+void loop() {
+int  distance = getDistance();   //variable to store the distance measured by the sensor
+
+  Serial.print(distance);     //print the distance that was measured
+  Serial.println(" in");      //print units after the distance
+
+  if(distance <= 6){                         //if the object is close
+    
+    //make the RGB LED green
+    digitalWrite(redPin, 0);
+    digitalWrite(greenPin, 225);
+    digitalWrite(bluePin, 0);
+    
+  } 
+   else{                                     //if the object is far away
+    
+    //make the RGB LED red
+    digitalWrite(redPin, 225);
+    digitalWrite(greenPin, 0);
+    digitalWrite(bluePin, 0);    
+  }
+
+  delay(50);      //delay 50ms between each reading
+}
+
+//------------------FUNCTIONS-------------------------------
+
+//RETURNS THE DISTANCE MEASURED BY THE HC-SR04 DISTANCE SENSOR
+
